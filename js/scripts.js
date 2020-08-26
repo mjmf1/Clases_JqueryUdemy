3a8009a95bfdedb174d565bcabb2aafcd62b8486
Marlon <marcomora61@gmail.com>
2020-08-25 20:26:30 -0500

    Agregando / Quitando clases con jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,39 @@
 $(function(){
     'use strict';
 
- $('main article:first').hide();
+  /*  $('div.logo img').addClass('activo');
+
+    $('.navegacion').show();
+
+    $('main article').addClass('activo');
+
+    $('.navegacion ul li a').on('click', function(e){
+        $('.navegacion ul li a').removeClass('activo');
+      e.preventDefault();
+      $(this).addClass('activo');
+
+    });*/
+
+   /* $('#menu').on('click', function(){
+        $('#navegacion').show();
+    });*/
+
+  /*  $('div.logo img').on('click',function(){
+$(this).remove();
+    });*
+  
+    /* $('div.logo img').on('click',function(){
+
+        console.log('has echo click en el logo');
+    });*/
+
+ /*$('main article:first').hide();*/
 
  /*var copia = $('main article:last').clone();
  $('main').prepend(copia);*/
 
- var copia = $('main article:last').clone();
+ /*var copia = $('main article:last').clone();
 
- $(copia).prependTo('main');
+ $(copia).prependTo('main');*/
 
 });
